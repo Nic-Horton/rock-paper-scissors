537de7504c0bf95a1e195b8e7d2c325e82cdaa55
Nic-Horton <77477203+Nic-Horton@users.noreply.github.com>
2023-01-24 12:56:04 -0600

    Create a rock paper scissors game first to 5

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getComputerChoice() {
+let playerScore = 0;
+let pcScore = 0;
+
+
+function getComputerChoice() { //Randomly generates computer choice
     const choiceArray = [
         "Rock", 
         "Paper", 
@@ -8,53 +12,78 @@ function getComputerChoice() {
     return computerSelection;
 }
 
-function playRound(playerSelection, computerSelection) {
-    
-    const player = playerSelection.toLowerCase();
-    const computer = computerSelection.toLowerCase();
-    
-    if ((player === "rock" && computer === "scissors") || (player === "paper" && computer === "rock") || (player === "scissors" && computer === "paper")) {
-        
-        const result = `You win! ${playerSelection} beats ${computerSelection}`;
-        return result;
+function disableButton() { 
+    buttons.forEach((button) => {
+        button.disabled = true;
+    });
+}
 
-    } else if ((computer === "rock" && player === "scissors") || (computer === "paper" && player === "rock") || (computer === "scissors" && player === "paper")) {
+function playRound(playerSelection) {
+    
+    const player = playerSelection;
+    const computer = getComputerChoice();
+    let result ='';
+    
+    
+    if ((player === "Rock" && computer === "Scissors") || (player === "Paper" && computer === "Rock") || (player === "Scissors" && computer === "Paper")) {
         
-        const result = `You lose... ${computerSelection} beats ${playerSelection}`;
-        return result;
+        console.log(computer);
+        playerScore++;
+        result = `<br>You win! ${playerSelection} beats ${computer}` + "<br><br>Player Score: " + playerScore + "<br>Computer Score: " + pcScore;
+        
+        
+        if (playerScore == 5) {
+            result +=("<br><br>The winner is the player!" + "<br><br> REFRESH TO PLAY AGAIN");
+            disableButton(); //Disables buttons to stop further input
+        }
+
+    } else if ((computer === "Rock" && player === "Scissors") || (computer === "Paper" && player === "Rock") || (computer === "Scissors" && player === "Paper")) {
+        
+        console.log(computer);
+        pcScore++;
+        result = `<br>You lose... ${computer} beats ${playerSelection}` + "<br><br>Player Score: " + playerScore + "<br>Computer Score: " + pcScore;
+        
+        
+        if (pcScore == 5) {
+            result += ("<br><br>The winner is the computer :(" + "<br><br> REFRESH TO PLAY AGAIN");
+            disableButton();
+        }
 
     } else {
 
-        const result = "It's a tie."
-        return result;
+        console.log(computer);
+        result = "<br>It's a tie." + "<br><br>Player Score: " + playerScore + "<br>Computer Score: " + pcScore;
 
     }
+    return (result);
 }
     
-function game() {
-    
-    let playerScore = 0;
-    let pcScore = 0;
-    for ( let i = 0; i < 5; i++) {
-        const playerSelection = prompt("What will it be... Rock, Paper, or Scissors");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection,computerSelection));
-        if (playRound(playerSelection,computerSelection) === `You win! ${playerSelection} beats ${computerSelection}`) {
-            playerScore++;
-            console.log(`player points: ${playerScore} computer points: ${pcScore}`);
-        } else if (playRound(playerSelection,computerSelection) === `You lose... ${computerSelection} beats ${playerSelection}`) {
-            pcScore++;
-            console.log(`player points: ${playerScore} computer points: ${pcScore}`);
-       } else {
-        console.log("Redo that Round");
-        i--
-       }
-    }
-    if (playerScore > pcScore) {
-            console.log("The winner is the player");
-        } else if (playerScore < pcScore) {
-            console.log("The winner is the computer");
-        } else {
-            console.log("It is a tie.")
-        }
-}
+
+const container = document.querySelector('#buttons');
+const textLine = document.querySelector('#textLine');
+
+const rockBtn = document.createElement('button');
+rockBtn.id = 'Rock';
+rockBtn.textContent = 'Rock';
+container.appendChild(rockBtn);
+
+const paperBtn = document.createElement('button');
+paperBtn.id = 'Paper';
+paperBtn.textContent = 'Paper';
+container.appendChild(paperBtn);
+
+const scissorsBtn = document.createElement('button');
+scissorsBtn.id = 'Scissors'
+scissorsBtn.textContent = 'Scissors';
+container.appendChild(scissorsBtn);
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        textLine.innerHTML = playRound(button.id);
+    });
+});
+
+
+
+
